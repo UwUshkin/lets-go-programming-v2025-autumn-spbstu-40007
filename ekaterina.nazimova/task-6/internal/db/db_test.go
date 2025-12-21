404ecdb55ab7a58278255a7c7f92abd83f14ea5d
UwUshkin <ekaterinanazimova07@gmail.com>
2025-12-21 03:06:31 +0300

    [TASK-6] ...

--- ekaterina.nazimova/task-6/internal/db/db_test.go
+++ ekaterina.nazimova/task-6/internal/db/db_test.go
@@ -5,28 +5,26 @@ import (
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/UwUshkin/task-6/internal/db"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
-	"github.com/UwUshkin/task-6/internal/db"
 )
 
 var (
 	errDbQuery = errors.New("db error")
-	errScan    = errors.New("scan error")
 	errIter    = errors.New("post-iteration error")
 )
 
 func TestGetNames(t *testing.T) {
 	t.Parallel()
-	dbConn, mock, err := sqlmock.New()
-	require.NoError(t, err)
-	defer dbConn.Close()
-
-	service := db.New(dbConn)
 
 	t.Run("success", func(t *testing.T) {
 		t.Parallel()
+		dbConn, mock, err := sqlmock.New()
+		require.NoError(t, err)
+		defer dbConn.Close()
+
+		service := db.New(dbConn)
 		rows := sqlmock.NewRows([]string{"name"}).AddRow("Alice").AddRow("Bob")
 		mock.ExpectQuery("SELECT name FROM users").WillReturnRows(rows)
 
@@ -37,41 +35,56 @@ func TestGetNames(t *testing.T) {
 
 	t.Run("query_error", func(t *testing.T) {
 		t.Parallel()
+		dbConn, mock, err := sqlmock.New()
+		require.NoError(t, err)
+		defer dbConn.Close()
+
+		service := db.New(dbConn)
 		mock.ExpectQuery("SELECT name FROM users").WillReturnError(errDbQuery)
 
-		_, err := service.GetNames()
+		_, err = service.GetNames()
 		require.Error(t, err)
 	})
 
 	t.Run("scan_error", func(t *testing.T) {
 		t.Parallel()
+		dbConn, mock, err := sqlmock.New()
+		require.NoError(t, err)
+		defer dbConn.Close()
+
+		service := db.New(dbConn)
 		rows := sqlmock.NewRows([]string{"name"}).AddRow(nil)
 		mock.ExpectQuery("SELECT name FROM users").WillReturnRows(rows)
 
-		_, err := service.GetNames()
+		_, err = service.GetNames()
 		require.Error(t, err)
 	})
 
 	t.Run("rows_err_after_loop", func(t *testing.T) {
 		t.Parallel()
+		dbConn, mock, err := sqlmock.New()
+		require.NoError(t, err)
+		defer dbConn.Close()
+
+		service := db.New(dbConn)
 		rows := sqlmock.NewRows([]string{"name"}).AddRow("Alice").RowError(0, errIter)
 		mock.ExpectQuery("SELECT name FROM users").WillReturnRows(rows)
 
-		_, err := service.GetNames()
+		_, err = service.GetNames()
 		require.Error(t, err)
 	})
 }
 
 func TestGetUniqueNames(t *testing.T) {
 	t.Parallel()
-	dbConn, mock, err := sqlmock.New()
-	require.NoError(t, err)
-	defer dbConn.Close()
-
-	service := db.New(dbConn)
 
 	t.Run("success", func(t *testing.T) {
 		t.Parallel()
+		dbConn, mock, err := sqlmock.New()
+		require.NoError(t, err)
+		defer dbConn.Close()
+
+		service := db.New(dbConn)
 		rows := sqlmock.NewRows([]string{"name"}).AddRow("UniqueName")
 		mock.ExpectQuery("SELECT DISTINCT name FROM users").WillReturnRows(rows)
 
@@ -82,27 +95,42 @@ func TestGetUniqueNames(t *testing.T) {
 
 	t.Run("query_error", func(t *testing.T) {
 		t.Parallel()
+		dbConn, mock, err := sqlmock.New()
+		require.NoError(t, err)
+		defer dbConn.Close()
+
+		service := db.New(dbConn)
 		mock.ExpectQuery("SELECT DISTINCT name FROM users").WillReturnError(errDbQuery)
 
-		_, err := service.GetUniqueNames()
+		_, err = service.GetUniqueNames()
 		require.Error(t, err)
 	})
 
 	t.Run("scan_error", func(t *testing.T) {
 		t.Parallel()
+		dbConn, mock, err := sqlmock.New()
+		require.NoError(t, err)
+		defer dbConn.Close()
+
+		service := db.New(dbConn)
 		rows := sqlmock.NewRows([]string{"name"}).AddRow(nil)
 		mock.ExpectQuery("SELECT DISTINCT name FROM users").WillReturnRows(rows)
 
-		_, err := service.GetUniqueNames()
+		_, err = service.GetUniqueNames()
 		require.Error(t, err)
 	})
 
 	t.Run("rows_err", func(t *testing.T) {
 		t.Parallel()
+		dbConn, mock, err := sqlmock.New()
+		require.NoError(t, err)
+		defer dbConn.Close()
+
+		service := db.New(dbConn)
 		rows := sqlmock.NewRows([]string{"name"}).AddRow("Alice").RowError(0, errIter)
 		mock.ExpectQuery("SELECT DISTINCT name FROM users").WillReturnRows(rows)
 
-		_, err := service.GetUniqueNames()
+		_, err = service.GetUniqueNames()
 		require.Error(t, err)
 	})
 }
